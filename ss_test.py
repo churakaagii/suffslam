2cdb9d54ab89c68dfac27627f01ad9cd45a34b89
Ada Nakama <anakama@bos-mpl1d.kendall.corp.akamai.com>
2017-04-28 09:10:24 -0400

    merging work from various computers

--- ss_test.py
+++ ss_test.py
@@ -22,3 +22,38 @@ def boardTest(board):
             print(fail[0], "connection to", fail[1], "not reciprocated")
 
 boardTest(board)
+
+def gen_units(amt):
+    return [x for x in range(amt)]
+
+def init_unit_list():
+    unit_list = {
+        "italian": gen_units(5),
+        "german": gen_units(5),
+        "irish": gen_units(4),
+        "russian": gen_units(6),
+        "anarchist": gen_units(3),
+        }
+    return unit_list
+
+class Unit():
+    def __init__(self, location):
+        self.location = location
+
+    def move(self, end_location):
+        if end_location in board[self.location]:
+            self.location = end_location
+        else:
+            return "invalid route"
+
+    def check_if_occupied(self, location):
+        #TODO: fix this broken for loop!
+        for gang in unit_list.values(): #TODO: add removal of current unit's gang from check
+            for unit in gang:
+                if unit.location == location:
+                    return "occupied"
+        return "clear"
+
+if __name__ == "__main__":
+    unit_list = init_unit_list()
+    print(unit_list)
